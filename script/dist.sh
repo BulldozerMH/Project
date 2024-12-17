fbbc79c1d1c2c82d2393c9232fe1b21bf28ea8ca
I <//>
2024-12-17 11:00:24 +0300

    dist

--- script/dist.sh
+++ script/dist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # copy dist
-#scp -r /home/ubuntu/dist ubuntu@10.1.10.249:/home/ubuntu
+scp -r /home/ubuntu/dist ubuntu@10.1.10.249:/home/ubuntu
 scp -r /home/ubuntu/dist ubuntu@10.1.10.250:/home/ubuntu
-#scp -r /home/ubuntu/dist ubuntu@10.1.10.251:/home/ubuntu
+scp -r /home/ubuntu/dist ubuntu@10.1.10.251:/home/ubuntu
 #scp -r /home/ubuntu/dist ubuntu@10.1.10.248:/home/ubuntu
