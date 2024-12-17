42f5b23ef4aaa56f6300d266643974135ca93082
I <//>
2024-12-17 10:43:38 +0300

    git

--- script/git.sh
+++ script/git.sh
@@ -4,9 +4,5 @@
 mkdir /home/ubuntu/Project
 cd /home/ubuntu/Project
 git init
-git config --global user.name "I"
-git config --global user.email "/</>"
-#git remote add Project git@github.com:BulldozerMH/Project.git main
-cd /home/.ssh
-ssh-keygen -t ed25519 -f SSH
-cat /home/.ssh/SSH.pub
+git remote add Project https://github.com/BulldozerMH/Project.git
+git pull Project main
