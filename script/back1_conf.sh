381a59cae0e4e1efc013ef9fd50b17085d4e75c7
I <//>
2024-12-18 17:15:07 +0300

    +

--- script/back1_conf.sh
+++ script/back1_conf.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+# back config
+
+sudo cp /home/ubuntu/Project/site/1.html /etc/www/1/index.html
+
+sudo cp /home/ubuntu/Project/Back/ports.conf /etc/apache2/ports.conf
+
+sudo cp /home/ubuntu/Project/Back/000-default.conf /etc/apache2/sites-available.conf
+
+sudo systemctl restart apache2.service
+
+sudo cp /home/ubuntu/Project/mysql/mysqld.cnf /etc/mysql/mysqld.cnf
+
+sudo systemctl restart mysql.service
