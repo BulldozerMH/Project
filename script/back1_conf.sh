59d0f3ce5bdc1b53402106a238992e54eb25cf7a
I <//>
2024-12-19 15:15:50 +0300

    mysql

--- script/back1_conf.sh
+++ script/back1_conf.sh
@@ -10,6 +10,14 @@ sudo cp /home/ubuntu/Project/Back/000-default.conf /etc/apache2/sites-available.
 
 sudo systemctl restart apache2.service
 
-sudo cp /home/ubuntu/Project/mysql/mysqld.cnf /etc/mysql/mysqld.cnf
+sudo cp /home/ubuntu/Project/mysql/b1_mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 
 sudo systemctl restart mysql.service
+
+sudo mysql
+
+CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'Back1_Repl2024';
+
+GRANT REPLICATION SLAVE ON *.* TO repl@'%';
+
+exit
