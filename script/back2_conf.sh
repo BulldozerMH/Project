20e2dc8f64ed56e4e3904ae0dcb1d6dbb3ea45c0
BulldozerMH <BulldozerMH@gmail.com>
2024-12-21 23:21:23 +0300

    Update back2_conf.sh

--- script/back2_conf.sh
+++ script/back2_conf.sh
@@ -2,11 +2,13 @@
 
 # back config
 
-sudo cp /home/ubuntu/Project/site/2.html /etc/www/2/index.html
+sudo mkdir /var/www/2
+
+sudo cp /home/ubuntu/Project/site/2.html /var/www/2/index.html
 
 sudo cp /home/ubuntu/Project/Back/ports.conf /etc/apache2/ports.conf
 
-sudo cp /home/ubuntu/Project/Back/000-default.conf /etc/apache2/sites-available.conf
+sudo cp /home/ubuntu/Project/Back/site2.conf /etc/apache2/sites-available/000-default.conf
 
 sudo systemctl restart apache2.service
 
@@ -14,12 +16,8 @@ sudo cp /home/ubuntu/Project/mysql/b2_mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.
 
 sudo systemctl restart mysql.service
 
-sudo mysql
+sudo mysql -e "STOP REPLICA;"
 
-STOP REPLICA;
+sudo mysql -e "CHANGE MASTER TO MASTER_HOST='10.1.10.250', MASTER_USER='repl', MASTER_PASSWORD='Back1_Repl2024', SOURCE_AUTO_POSITION=1, GET_MASTER_PUBLIC_KEY = 1;"
 
-CHANGE MASTER TO MASTER_HOST='10.1.10.250', MASTER_USER='repl', MASTER_PASSWORD='Back1_Repl2024', SOURCE_AUTO_POSITION=1, GET_MASTER_PUBLIC_KEY = 1;
-
-START SLAVE;
-
-exit
+sudo mysql -e "START SLAVE;"
