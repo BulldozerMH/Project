50c067d9fc93f73d8b7c570487485962271e9d1d
BulldozerMH <BulldozerMH@gmail.com>
2024-12-21 23:07:09 +0300

    Update back1_conf.sh

--- script/back1_conf.sh
+++ script/back1_conf.sh
@@ -4,7 +4,7 @@
 
 sudo mkdir /var/www/1
 
-sudo cp /home/ubuntu/Project/site/1.html /etc/var/1/index.html
+sudo cp /home/ubuntu/Project/site/1.html /var/1/index.html
 
 sudo cp /home/ubuntu/Project/Back/ports.conf /etc/apache2/ports.conf
 
@@ -16,10 +16,8 @@ sudo cp /home/ubuntu/Project/mysql/b1_mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.
 
 sudo systemctl restart mysql.service
 
-sudo mysql
+sudo mysql -e CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'Back1_Repl2024';
 
-CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'Back1_Repl2024';
-
-GRANT REPLICATION SLAVE ON *.* TO repl@'%';
+sudo mysql -e GRANT REPLICATION SLAVE ON *.* TO repl@'%';
 
 exit
