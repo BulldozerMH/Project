bad5d688d6772db57735856545c94530a82f4bcf
BulldozerMH <BulldozerMH@gmail.com>
2024-12-23 14:41:29 +0300

    Update git.sh

--- script/git.sh
+++ script/git.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# disk space
+
+sudo lvextend -l +100%FREE /dev/mapper/ubuntu--vg-ubuntu--lv
+
+sudo resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
+
 # git script
 mkdir /home/ubuntu/Project
 cd /home/ubuntu/Project
