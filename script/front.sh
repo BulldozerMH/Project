1c8c46a2e34415abb8b443ae39e3f5a50bc3b550
BulldozerMH <BulldozerMH@gmail.com>
2024-12-23 23:37:09 +0300

    Update front.sh

--- script/front.sh
+++ script/front.sh
@@ -11,7 +11,7 @@ sudo apt install prometheus prometheus-node-exporter -y
 
 sudo apt install -y adduser libfontconfig1 musl
 
-sudo dpkg -i /home/ubuntu/dist/grafana_11.3.1_amd64.deb
+sudo dpkg -i /home/ubuntu/dist/grafana_10.0.3_amd64.deb
 
 sudo systemctl daemon-reload
 sudo systemctl enable grafana-server
