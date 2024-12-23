e19d4da3d6225e80f9ed8a96b938563ad5fe8e10
BulldozerMH <BulldozerMH@gmail.com>
2024-12-23 13:42:57 +0300

    Update front.sh

--- script/front.sh
+++ script/front.sh
@@ -8,8 +8,6 @@ sudo apt install nginx -y
 
 sudo apt install prometheus-node-exporter -y
 
-sudo dpkg -i /home/ubuntu/dist/filebeat-8.9.1-amd64.deb
-
 sudo apt install -y adduser libfontconfig1 musl
 
 sudo dpkg -i /home/ubuntu/dist/grafana_11.3.1_amd64.deb
