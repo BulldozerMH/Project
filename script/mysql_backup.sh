73cc8fc082662cbd344c2e58bd6fe64c21ac548a
BulldozerMH <BulldozerMH@gmail.com>
2024-12-23 12:45:33 +0300

    Update mysql_backup.sh

--- script/mysql_backup.sh
+++ script/mysql_backup.sh
@@ -12,7 +12,7 @@ sudo mkdir /home/ubuntu/mysql_backup
 
 table_dest=//home/ubuntu/mysql_backup
 
-for table in `mysql -u root -N -B -e 'show tables from $db'`;
+for table in `mysql -u root -N -B -e 'show tables from '$db''`;
 do
     echo "Создан бэкап таблицы $table"
     mysqldump --add-drop-table --add-locks --create-options --disable-keys --extended-insert --single-transaction --quick --set-charset --events --routines --triggers --set-gtid-purged=OFF --source-data=2 -u root $db $table > $table_dest/$table.sql
